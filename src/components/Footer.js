79aaf4aa17d82780706c816934a9378cdb898c91
Patricia <patriciamvbog@gmail.com>
2024-03-02 02:28:30 +0000

    add footer component and buttons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,44 @@
 import React from 'react';
+import Logo from '../images/Logo.png'
 
 function Footer() {
     return (
 
-        <div>
-            Footer
-        </div>
+       <footer>
+        <section>
+            <div className='company-info'>
+                <img src={Logo} alt="logo" className='logo-small' />
+                <p>We are a Family Indian restaurant, focused on traditional recipes served with a modern twist</p>
+            </div>
+            <div>
+                <h3>Important Links</h3>
+                <ul>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/">About</a></li>
+                    <li><a href="/">Menu</a></li>
+                    <li><a href="/">Reservations</a></li>
+                    <li><a href="/">Order Online</a></li>
+                    <li><a href="/">Login</a></li>
+                </ul>
+            </div>
+            <div>
+                <h3>Contact</h3>
+                <ul>
+                    <li>Address: 123 Cork, IE </li>
+                    <li>Phone: +123 234 3445</li>
+                    <li>Email: esmeraldcurry@resto.com</li>
+                </ul>
+            </div>
+                <div>
+                    <h3>Social Media Links</h3>
+                    <ul>
+                        <li><a href="/">Facebook</a> </li>
+                        <li><a href="/">Instagram</a></li>
+                        <li><a href="/">Twitter</a></li>
+                    </ul>
+                </div>
+        </section>
+       </footer>
     );
 }
 
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,7 +1,30 @@
 import React from 'react';
 import recipes from '../recipes';
+import Swal from 'sweetalert2'
 
 const Menu = () => {
+
+const handleOrder = (id) => {
+    console.log(id, "id is clicked")
+    Swal.fire({
+        title: "Are you sure?",
+        text: "You won't be able to revert this!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, order now!"
+    }).then((result) => {
+        if (result.isConfirmed) {
+            Swal.fire({
+                title: "Order placed! ",
+                text: "You Menu has been ordered",
+                icon: "success"
+            });
+        }
+    });
+}
+
     return (
 
         <div className='menu-container'>
@@ -22,7 +45,7 @@ const Menu = () => {
                             <p>{recipe.price}</p>
                         </div>
                         <p>{recipe.description}</p>
-                        <button className='orderBtn'>Order Now</button>
+                        <button className='orderBtn' onClick={() => handleOrder(recipe.id)}>Order Now</button>
                     </div>
                    </div>) 
                 }
