436891688f97c92395480fe9833ecf20e24e0371
Patricia <patriciamvbog@gmail.com>
2024-03-03 00:49:53 +0000

    add image in header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import bannerImg from '../images/restaurantFood.jpg';
 
 function Header() {
     return (
         <header className='header'>
               <section>
+                {/* banner texts */}
                 <div className='banner'>
                     <h2>Emerald Curry House</h2>
                     <h3>Cork</h3>
                     <p>We are a family owned Indian restaurant on traditional recipes served with a modern twist. </p>
                     <Link to="/booking"><button aria-label='On Click'>Reserve table</button></Link>
                 </div>
+                {/* banner image */}
+                <div className='banner-img'>
+                    <img src={bannerImg} alt="banner" />
+                </div>
+
               </section>
         </header>
     );
